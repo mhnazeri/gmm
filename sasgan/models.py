FDE & ADE averagig fixed, gpu logging fixed. ready to go

sasgan/models.py
@@ -229,10 +229,6 @@ class Fusion(nn.Module):
 ##################################################################################
 #                                    Decoder
 # ________________________________________________________________________________
-# Todo: for later
-#       1. implement the pooling every timestep mechanism ---> Done
-#       2. Not sure about adding for all the features ----> Done
-
 class Decoder(nn.Module):
     def __init__(self,
                  fusion_length,
@@ -397,7 +393,7 @@ class TrajectoryGenerator(nn.Module):
         """
         :param obs_traj: shape (obs_length, batch, inputs_size)
         :param obs_traj_rel: shape (obs_length, batch, inputs_size)
-        :param frames: Tensor of shape (4, 1, 256, 256)
+        :param frames: Tensor of shape (batch, 4, 1, 256, 256)
         :return: final_prediction: shape (seq_length, batch, input_size)
         """
         batch_size = obs_traj.shape[1]

sasgan/train.py
@@ -25,7 +25,7 @@ from models import \
 parser = argparse.ArgumentParser()
 
 # Keep these
-parser.add_argument("--use_gpu", default=0, type=int)
+parser.add_argument("--use_gpu", default=1, type=int)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -39,7 +39,7 @@ TRAINING = config("Training")
 GENERATOR = config("Generator")
 DISCRIMINATOR = config("Discriminator")
 
-summary_writer_general = SummaryWriter(DIRECTORIES["log"])
+summary_writer_validation = SummaryWriter(os.path.join(DIRECTORIES["log"], "validation_loss"))
 summary_writer_generator = SummaryWriter(os.path.join(DIRECTORIES["log"], "generator"))
 summary_writer_discriminator = SummaryWriter(os.path.join(DIRECTORIES["log"], "discriminator"))
 summary_writer_cae = SummaryWriter(os.path.join(DIRECTORIES["log"], "cae"))
@@ -75,8 +75,6 @@ def get_cae(device):
                                         ignore_first_epochs=int(CAE["ignore_first_epochs"]),
                                         device=device)
 
-
-    logger.info("Done training/loading the CAE!")
     return cae_encoder, cae_decoder
 
 
@@ -85,11 +83,10 @@ def main():
     if args.use_gpu:
         device = get_device(logger)
     else:
+        logger.info("Forced to use cpu as device...")
         device = torch.device("cpu")
 
     cae_encoder, cae_decoder = get_cae(device)
-    # By now the Encoder and the Decoder have been loaded or trained
-
     logger.info("Preparing the dataloader for the main model...")
 
     nuscenes_data = NuSceneDataset(root_dir=DIRECTORIES["train_data"])
@@ -101,9 +98,10 @@ def main():
 
     embedder = None
     if bool(GENERATOR["use_cae_encoder"]):
+        logger.info("Using the CAE enocder...")
         embedder = cae_encoder
 
-    logger.info("building the GAN...")
+    logger.info("Constructing the GAN...")
 
     # Construct the models
     g = TrajectoryGenerator(
@@ -121,8 +119,8 @@ def main():
         context_feature_model_arch=str(GENERATOR["context_feature_model_arch"]),
         num_layers=int(GENERATOR["num_layers"])
     )
-    logger.info("Here is the generator")
-    logger.info(g)
+    logger.debug("Here is the generator...")
+    logger.debug(g)
 
     d = TrajectoryDiscriminator(
         embedder=cae_encoder,
@@ -136,8 +134,8 @@ def main():
         dropout=float(DISCRIMINATOR["dropout"])
         )
 
-    logger.info("Here is the discriminator")
-    logger.info(d)
+    logger.debug("Here is the discriminator...")
+    logger.debug(d)
 
     # Initialize the weights
     g.apply(init_weights)
@@ -170,8 +168,7 @@ def main():
         start_epoch = loaded_dictionary["epoch"] + 1
         step = loaded_dictionary["step"]
         best_ADE_loss = loaded_dictionary["best_ADE_loss"]
-        g_loss = loaded_dictionary["current_g_loss"]
-        d_loss = loaded_dictionary["current_d_loss"]
+
         logger.info(f"Done loading the model in {loading_path}")
 
     else:
@@ -179,8 +176,6 @@ def main():
         step = 0
         start_epoch = 0
         best_ADE_loss = inf
-        d_loss = inf
-        g_loss = inf
 
     logger.debug("Training the model")
     for epoch in range(start_epoch, start_epoch + int(TRAINING["epochs"])):
@@ -216,7 +211,7 @@ def main():
                 g_loss = bce_loss(fake_prediction, true_labels)
                 g_losses.append(g_loss.item())
 
-                summary_writer_generator.add_scalar("Loss", g_loss, step)
+                summary_writer_generator.add_scalar("GAN_loss", g_loss, step)
 
                 g_optimizer.zero_grad()
                 g_loss.backward()
@@ -239,7 +234,7 @@ def main():
                 d_loss = fake_loss + real_loss
                 d_losses.append(d_loss.item())
 
-                summary_writer_discriminator.add_scalar("Loss", d_loss, step)
+                summary_writer_discriminator.add_scalar("GAN_loss", d_loss, step)
 
                 d_optimizer.zero_grad()
                 d_loss.backward()
@@ -249,14 +244,17 @@ def main():
             logger.debug(f"step {step} finished!")
             step += 1
 
+        ##########################################################################################
+        #                            evaluating the trained model
+        ##########################################################################################
         with torch.no_grad():
             logger.debug("Evaluating the model")
             g.eval()
             d.eval()
 
             fake_traj = g(batch["past"], batch["rel_past"], batch["motion"])
-            ADE_loss = displacement_error(fake_traj, batch["future"]).item()
-            FDE_loss = final_displacement_error(fake_traj[-1], batch["future"][-1]).item()
+            ADE_loss, _ = displacement_error(fake_traj, batch["future"]).item()
+            FDE_loss, _ = final_displacement_error(fake_traj[-1], batch["future"][-1]).item()
 
             # Todo: show some qualitative results of the predictions to be shown in tensorboard
 
@@ -269,14 +267,16 @@ def main():
                 f"ADE_loss:{ADE_loss:.2f}\t\t"
                 f"FDE_loss:{FDE_loss:.2f}")
 
-        summary_writer_general.add_scalar("ADE_loss", ADE_loss, epoch)
-        summary_writer_general.add_scalar("FDE_loss", FDE_loss, epoch)
+        summary_writer_validation.add_scalar("ADE_loss", ADE_loss, epoch)
+        summary_writer_validation.add_scalar("FDE_loss", FDE_loss, epoch)
 
-        # check if it is time to save a checkpoint of the model
+        ##########################################################################################
+        #                                   Saving the model
+        ##########################################################################################
         if (ADE_loss <= best_ADE_loss and epoch > int(TRAINING["ignore_first_epochs"])) or \
                 (epoch + 1) % int(TRAINING["save_every_d_steps"]) == 0 or \
                 (epoch + 1) == start_epoch + int(TRAINING["epochs"]):
-            logger.info("Saving the model....")
+
             checkpoint = {
                 "epoch": epoch,
                 "step": step,
@@ -285,16 +285,14 @@ def main():
                 "g_optimizer": g_optimizer.state_dict(),
                 "d_optimizer": d_optimizer.state_dict(),
                 "best_ADE_loss": best_ADE_loss,
-                "current_d_loss": d_loss,
-                "current_g_loss": g_loss,
             }
             if ADE_loss <= best_ADE_loss and epoch > int(TRAINING["ignore_first_epochs"]):
+                logger.info("Saving the model(lowest ADE loss)...")
                 best_ADE_loss = ADE_loss
                 torch.save(checkpoint, save_dir + "/best.pt")
 
-            if (epoch + 1) % int(TRAINING["save_every_d_steps"]) == 0 or \
-                    (epoch + 1) == start_epoch + int(TRAINING["epochs"]):
-
+            else:
+                logger.info(f"Saving the model(intervals)...")
                 torch.save(checkpoint, save_dir + "/checkpoint-" + str(epoch + 1) + ".pt")
 
 if __name__ == '__main__':